49c8b58aab67205f5437c6fc2acc08197c242250
mdulcio <mdulcio@visiostack.com>
2025-12-14 22:09:31 -0500

    feat: Fix timer

--- src/lib/app/concepts/Book/ChapterProgressView.svelte
+++ src/lib/app/concepts/Book/ChapterProgressView.svelte
@@ -6,10 +6,26 @@
 	import { clamp } from 'es-toolkit';
 
 	let { chapter }: { chapter: Chapter } = $props();
-	let progressPercent = $derived.by(() => {
-		return clamp((chapter.player.currentTime ?? 0) / (chapter.player.duration ?? 1), 0, 1) * 100;
-	});
 	let wrapperElement = $state<HTMLElement>();
+	let timerElementWidth = $state<number>();
+	let progressPercent = $derived.by(() => {
+		const realPercent = clamp(
+			(chapter.player.currentTime ?? 0) / (chapter.player.duration ?? 1),
+			0,
+			1
+		);
+
+		const maxWidth = (wrapperElement?.clientWidth ?? 0) - (timerElementWidth ?? 0) / 2;
+
+		const timerWidthPercent = (timerElementWidth ?? 0) / 2 / (wrapperElement?.clientWidth ?? 1);
+		const adjusted = clamp(
+			realPercent,
+			timerWidthPercent,
+			maxWidth / (wrapperElement?.clientWidth ?? 1)
+		);
+
+		return adjusted * 100;
+	});
 </script>
 
 <div class="relative flex w-full" bind:this={wrapperElement}>
@@ -22,6 +38,7 @@
 		{#snippet indicator({ parentElement })}
 			<Portal to={wrapperElement}>
 				<button
+					bind:clientWidth={timerElementWidth}
 					class="absolute top-0 -translate-x-1/2 -translate-y-full"
 					style:left="{progressPercent}%"
 				>
